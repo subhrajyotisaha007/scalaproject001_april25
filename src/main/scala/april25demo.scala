3d2f6aa0978e967f13ff6a53c5fec3cf8b140ae9
subhrajyoti <subhrajyotisaha32@gmail.com>
2026-04-28 11:45:47 +0530

    adding code changes second time

--- src/main/scala/april25demo.scala
+++ src/main/scala/april25demo.scala
@@ -3,7 +3,38 @@ object april25demo {
   def main(args:Array[String]):Unit={
 
 
-    print("hello World")
+
+//    print("hello World")
+    val a = 5
+//    val a:Int= 5
+
+    var b = 80
+    var d = 90
+//        b = 80
+//
+//    val c:String = "9"
+//
+    println(a>b)
+
+    println(a+b)
+    val c:Float = (b/a)
+    println(b/a)
+    println(b/a.toDouble)     //kind of typecast
+    println(c)
+    println(a-b)
+    println(a*b)
+    println(a%b)
+
+    println(a>b && a>d)
+    println(a>b || a>d)
+
+    print(b,c)
+    //println(a>b && a>d)
+
+
+
+
+
 
 
 
